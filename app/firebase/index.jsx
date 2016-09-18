4c150966e47e554d01d0589a7aabba47ff4da03c
David Wilbanks <dwilbank@gmail.com>
2016-09-18 19:27:12 -0400

    tests failing

--- app/firebase/index.jsx
+++ app/firebase/index.jsx
@@ -2,11 +2,10 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyABUDQ37aRaBMceh8iyxNDA3xjtKfKrEOI",
-        authDomain: "todos-redux-bfe03.firebaseapp.com",
-        databaseURL: "https://todos-redux-bfe03.firebaseio.com",
-        storageBucket: "todos-redux-bfe03.appspot.com",
-        messagingSenderId: "1061946244582"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET
     };
     firebase.initializeApp(config);
 } catch (e) {
